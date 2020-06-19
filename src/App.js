a9ba13ea75093dc7884bbeda685f6ef43c8ca1ff
SUBHAM <subham040800@gmail.com>
2020-06-19 16:12:05 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
 
     return (
       <div className={styles.container}>
-        <img className={styles.image} src={coronaImage} />
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Charts data={data} country={country} />
